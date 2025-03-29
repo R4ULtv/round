270453516c0818474a82aace2e40fc5e7d6d1e53
Raul Carini <85316240+R4ULtv@users.noreply.github.com>
2025-03-29 16:12:13 +0100

    Add theme toggle button component

---
+++ components/theme-switch.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { useTheme } from "next-themes";
+import { useState, useEffect } from "react";
+
+export default function ThemeSwitch() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const toggleTheme = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  return (
+    <Button size="sm" variant="ghost" onClick={toggleTheme}>
+      {theme === "light" ? <MoonIcon /> : <SunIcon />}
+    </Button>
+  );
+}
